5c0be0224e0c57b950d096a13b3b320e20e19787
muhammadalinoor-1982 <noor.web.journey@gmail.com>
2023-03-14 14:06:49 +0600

    Documantation Done

--- resources/views/qtec/pages/wordCount.blade.php
+++ resources/views/qtec/pages/wordCount.blade.php
@@ -15,8 +15,15 @@
                         {
                             echo '<b>'.$word.'&nbsp;occurrences are&nbsp;'.substr_count(strtolower($string),$word)."&nbsp;times.</b><br/>";
                         }
-
                 @endphp
+                <br><br>
+                <p>
+                    Documentation about this task: <br>
+                    1. First of all Take a Array and mention the value of array <br>
+                    2. Then take a string for main text <br>
+                    3. Then take a "foreach loop" to travers main text string <br>
+                    4. Then check Array value through this function "substr_count(strtolower($string),$word)"
+                </p>
 </div>
 </div>
 <!--Export Data Table End-->
